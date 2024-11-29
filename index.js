a5f38c9611df3c4a1a03a1130ebbf56ea04ab6a8
krishnaawate8@gmail.com <krishnaawate8@gmail.com>
2024-11-29 22:24:14 +0530

    Cors issue

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require("./database/mongoose");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 // Error handling function
 const { globalErrorHandler } = require("./utils/error");
